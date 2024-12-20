58c12dfd1f57f7e0722c4aa4bd1f84f629c567f2
stephano47 <stephanomelv@gmail.com>
2024-12-20 01:34:02 -0500

    Phase 4 Complete

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EasyShop Update V2
 ## December 20th, 2024
+![EasyShop](https://github.com/stephano47/EasyShop/blob/main/src/photos/Screenshot%202024-12-19%20134111.png?raw=true)
 
 Hey there,
 
@@ -11,13 +12,14 @@ We're excited to share the latest update for EasyShop! In this version, we've fo
 You may have noticed that the product search was returning incorrect results. We've fixed this issue, so you should now get the correct products when searching.
 
 ### Duplication of Items:
-Some products were appearing duplicated with only minimal differences. We've identified the cause of the bug and resolved it in this update.
+Some products were appearing duplicated with only minimal differences. This was due to the method using create instead of update. 
 
 ## Bugs Found
 
 1. **Category Load Failure**:
     - I noticed while testing the EasyShop site that occasionally, the category loading might fail.
     - If this happens, refreshing the page fixes the issue.
-    - This happens whenever I went into the cart section for a couple of seconds then head back into the home screen.
+    - This happens whenever I went into the cart section for a couple of seconds then head back into the home screen.\
+![LoadFail](https://github.com/stephano47/EasyShop/blob/main/src/photos/Screenshot%202024-12-19%20092948.png?raw=true)
 
  
--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,60 @@
 package org.yearup.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import java.security.Principal;
+
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
 public class ProfileController {
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao){
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @PreAuthorize("permitAll()")
+    @PostMapping()
+    public Profile create(@RequestBody Profile profile){
+        return profileDao.create(profile);
+    }
+
+    @GetMapping()
+    public Profile getByUserId(Principal principal){
+
+        String username = principal.getName();
+        User user = userDao.getByUserName(username);
+
+        // get profile by id
+        Profile profile = profileDao.getByUserId(user.getId());
+        if (profile == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return profile;
+    }
+
+    @PutMapping()
+    public Profile updateProfile(@RequestBody Profile profile, Principal principal) {
+
+        String username = principal.getName();
+        int userId = userDao.getByUserName(username).getId();
+        profile.setUserId(userId);
+        // Perform the update in the DAO layer and fetch the updated profile
+        Profile updatedProfile = profileDao.update(profile);
+        // Return the updated profile to the client
+        return updatedProfile;
+    }
 }
